323b3683891d9d86135663124fa6095646424a22
marjciardullo <marjoryeramirez@gmail.com>
2022-03-11 20:38:08 -0300

    3 files modified

--- src/app/list.ts
+++ src/app/list.ts
@@ -1,14 +1,49 @@
 export const listData = [
   {
     id: 1,
-    text: 'Text Um',
+    materia: 'Negócios e Marketing e Eletrônicos',
+    dia: 'Quarta-feira',
+    horario: '19h às 22h30',
+    dados: 'oi',
+    ementa:
+      'Conceitos de marketing e de marketing digital. O comportamento do consumidor on-line. Marketing de conteúdo. Marketing nas mídias sociais. E-mail marketing. Marketing viral. Publicidade on-line. Pesquisa de marketing on-line. Marketing e os negócios na Internet. Estudos de caso',
+    objetivo:
+      'Apresentar e discutir as questões do marketing na era da Internet. Abordar as principais estratégias, ações, ferramentas e mídias presentes na Web. Objetivos específicos. Compreender e utilizar a Internet para os negócios e implementar ações de marketing, comunicação e publicidade on-line',
   },
   {
     id: 2,
-    text: 'Texto Dois',
+    materia: 'Desenvolvimento para Servidores II',
+    dia: 'Quinta-feira',
+    horario: '19h às 22h30',
   },
   {
     id: 3,
-    text: 'Texto Três',
+    materia: 'Inglês V',
+    dia: 'Terça-feira',
+    horario: '19h às 20h40',
+  },
+  {
+    id: 4,
+    materia: 'Projeto de Prototipagem e Testes de Usabilidade',
+    dia: 'Sábado',
+    horario: '8h às 11h30',
+  },
+  {
+    id: 5,
+    materia: 'Projeto de Encontrabilidade',
+    dia: 'Segunda-feira',
+    horario: '19h às 22h30',
+  },
+  {
+    id: 6,
+    materia: 'Tópicos Especiais em Sistemas para Internet II',
+    dia: 'Sexta-feira',
+    horario: '19h às 22h30',
+  },
+  {
+    id: 7,
+    materia: 'Projeto de Trabalho de Graduação em Sistemas para Internet I',
+    dia: 'Quarta-feira',
+    horario: '11h30 às 13h',
   },
 ];
